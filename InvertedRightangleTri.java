6f3d2c0058635802ccc360194b90e15a12c438d3
SADAVAMSHI <sadavamshimancholla@gmail.com>
2026-04-02 23:58:09 +0530

    Inverted pattern

---
+++ InvertedRightangleTri.java
@@ -0,0 +1,12 @@
+public class InvertedRightangleTri {
+    public static void main(String[] args) {
+        for(int i=0;i<=4;i++){
+            for(int j=i;j<=4;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+    
+}
