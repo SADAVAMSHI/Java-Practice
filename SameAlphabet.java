f6b4d673707024b32e9609f17d417cc7b2c8d2e1
SADAVAMSHI <sadavamshimancholla@gmail.com>
2026-04-08 23:42:22 +0530

    SameAlphabet

---
+++ SameAlphabet.java
@@ -0,0 +1,13 @@
+public class SameAlphabet {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            char ch = 'S';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}
